cb273d7e84fe83ac5a8b456d208abe20564fee62
Mawusime Blewuada <mawusi@piranhabar.ie>
2018-02-04 20:07:57 +0000

    Outlined model classes

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-# LoginForm
-	# username or email, password, submit, forgot password link
-# RegistrationForm
-	# username, email, password field, retype password field, submit
-# ChallengeForm
-	# answer, submit
-# EditProfileForm
-	# username, email, password field, retype password field, submit
--- app/models.py
+++ app/models.py
@@ -1,16 +1,20 @@
-# Define structure of database in database models
-from app import db
+S# Define structure of database in database models
+from app import db, login
 import sys
 import os
+from flask_login import UserMixin, login_manager
 
-class User(db.model):
+@login_manager.user_loader
+def load_user(id):
+    return User.get(int(id))
+
+class User(UserMixin, db.model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 	password_hash = db.Column(db.String(128))
-	complete_categories = db.Column(db.String(120), unique=False, nullable=False)
-	complete_lessons = db.Column(db.String(120), unique=False, nullable=False)
-	pos_statistics = db.relationship('POSStatistic', backref='user', lazy=True)
+	complete_categories = category_ids
+	complete_lessons = lesson_ids
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,36 +27,61 @@ class Category(db.model):
 class Lesson(db.model):
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer, unique=False, nullable=False)
-    parts_of_speech = db.relationship('PartOfSpeech', backref='lesson', lazy=True)
+    vocab = db.relationship('Vocabulary', backref='lesson', lazy=True)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
-class PartOfSpeech(db.model):
+class Vocabulary(db.model):
 	id = db.Column(db.Integer, primary_key=True)
 	l1 = db.Column(db.String(64), nullable=False)
 	l2 = db.Column(db.String(64), nullable=False)
 	lexical = db.Column(db.String(64), nullable=False)
+	grammar = db.Column(db.String(64), nullable=False)
 	audio = db.Column(db.String(240), nullable=True)
 	image = db.Column(db.String(240), nullable=True)
 	lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'), nullable=False)
 
-class UserStatistic(db.model):
+class Sentence(db.model):
 	id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	l1 = db.Column(db.String(64), nullable=False)
+	l2 = db.Column(db.String(64), nullable=False)
+	grammars = db.Column(db.String(64), nullable=False)
+	audio = db.Column(db.String(240), nullable=True)
+	image = db.Column(db.String(240), nullable=True)
+	vocab_ids = db.relationship()
 
-    def __repr__(self):
-    	#return '<PartOfSpeech {} , {}>'.format(self.l2, self.lexical)
+# Association
+class UserCategory(db.model):
+    user_id
+    lesson_id
+    available
+    completed
 
-class POSStatistic(db.model):
-	id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+# Association
+class UserLesson(db.model):
+    user_id
+    lesson_id
+    available
+    completed
+
+# Association
+class UserVocabulary(db.model):
+	user_id
+	vocab_id
+	introduced
+	difficulty
 	last accessed = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
 	num_accessed = db.Column(db.Integer, unique=False, nullable=False)
 	num_incorrect = db.Column(db.Integer, unique=False, nullable=False)
-	#difficulty
-	pos_id = db.Column(db.Integer, db.ForeignKey('partofspeech.id'), unique=True, nullable=False)
 
-    def __repr__(self):
-    	return '<PartOfSpeech {} , {}>'.format(self.l2, self.lexical)
+# Association
+class UserSentence(db.model):
+	user_id
+	sentence_id
+	available
+	introduced
+	last accessed
+	num_accessed
+	num_incorrect
 
 #class Challenge:
 	# ID, category ID, type, question, answer, challenge (multiple choice, mixed words)
