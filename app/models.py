cf04b0dbd492a578f7e5f99e64198246f5f2e358
Mawusi Blewuada <mousekoz13@yahoo.co.uk>
2018-02-18 17:17:02 +0000

    Wrote model classes

--- app/models.py
+++ app/models.py
@@ -1,87 +1,69 @@
-S# Define structure of database in database models
+# Define structure of database in database models
 from app import db, login
 import sys
 import os
-from flask_login import UserMixin, login_manager
+from flask_login import UserMixin
 
-@login_manager.user_loader
+#Association Tables:
+
+lexical_classes = db.Table('lexical_classes', db.Column('ewe_vocab_id', db.Integer, db.ForeignKey('ewevocabulary.id')), 
+												db.Column('lexical_name', db.String(64), db.ForeignKey('lexicalclass.name')))
+
+grammar_types = db.Table('grammar_types', db.Column('ewe_vocab_id', db.Integer, db.ForeignKey('ewevocabulary.id')), 
+												db.Column('grammar_type', db.String(64), db.ForeignKey('grammartype.name')))
+
+@login.user_loader
 def load_user(id):
     return User.get(int(id))
 
-class User(UserMixin, db.model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-	password_hash = db.Column(db.String(128))
-	complete_categories = category_ids
-	complete_lessons = lesson_ids
+	#password_hash = db.Column(db.String(128))
+	#vocab_accessed = relationship to UserVocabulary tble
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class Category(db.model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True, nullable=False)
-    lessons = db.relationship('Lesson', backref='category', lazy=True)
+class Category(db.Model):
+    name = db.Column(db.String(64), primary_key=True)
 
-class Lesson(db.model):
-    id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.Integer, unique=False, nullable=False)
-    vocab = db.relationship('Vocabulary', backref='lesson', lazy=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-
-class Vocabulary(db.model):
+class EweVocabulary(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	l1 = db.Column(db.String(64), nullable=False)
-	l2 = db.Column(db.String(64), nullable=False)
-	lexical = db.Column(db.String(64), nullable=False)
-	grammar = db.Column(db.String(64), nullable=False)
+	word = db.Column(db.String(64), nullable=False)
+	category = db.Column(db.String(64), db.ForeignKey('category.name'), nullable=False)
+	lexical_classes = db.relationship('LexicalClass', secondary=lexical_classes, lazy='dynamic', backref=db.backref('ewe_vocabulary', lazy='dynamic'))
+	grammar_types = db.relationship('GrammarType', secondary=grammar_types, lazy='dynamic', backref=db.backref('ewe_vocabulary', lazy='dynamic'))
 	audio = db.Column(db.String(240), nullable=True)
 	image = db.Column(db.String(240), nullable=True)
-	lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'), nullable=False)
+	english_words  = db.relationship('EnglishWords', backref='ewe_vocabulary', lazy='dynamic')
 
-class Sentence(db.model):
+class LexicalClass(db.Model):
+	name = db.Column(db.String(64),  primary_key=True)
+
+class GrammarType(db.Model):
+	name = db.Column(db.String(64),  primary_key=True)
+
+class EnglishWords(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	l1 = db.Column(db.String(64), nullable=False)
-	l2 = db.Column(db.String(64), nullable=False)
-	grammars = db.Column(db.String(64), nullable=False)
-	audio = db.Column(db.String(240), nullable=True)
-	image = db.Column(db.String(240), nullable=True)
-	vocab_ids = db.relationship()
+	word = db.Column(db.String(64), nullable=False)
+	ewe_vocab_id = db.Column(db.Integer, db.ForeignKey('ewevocabulary.id'))
 
-# Association
-class UserCategory(db.model):
-    user_id
-    lesson_id
-    available
-    completed
 
-# Association
-class UserLesson(db.model):
-    user_id
-    lesson_id
-    available
-    completed
-
-# Association
-class UserVocabulary(db.model):
+""""
+# Association table
+class UserVocabulary(db.Model):
 	user_id
-	vocab_id
+	ewe_vocab_id 
 	introduced
 	difficulty
-	last accessed = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
+	#last accessed = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
 	num_accessed = db.Column(db.Integer, unique=False, nullable=False)
 	num_incorrect = db.Column(db.Integer, unique=False, nullable=False)
+"""
 
 # Association
-class UserSentence(db.model):
-	user_id
-	sentence_id
-	available
-	introduced
-	last accessed
-	num_accessed
-	num_incorrect
 
 #class Challenge:
 	# ID, category ID, type, question, answer, challenge (multiple choice, mixed words)
