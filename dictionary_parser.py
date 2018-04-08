960147756f8f0ae00db87b7ea858c743981129ea
Mawusi Blewuada <mousekoz13@yahoo.co.uk>
2018-04-08 16:18:07 +0100

    Various updates, but look at dictionaryparser2.py for further development.

--- dictionary_parser.py
+++ dictionary_parser.py
@@ -1,70 +1,109 @@
 from app import db
-from app.models import EweVocabulary, EnglishWords, LexicalClass, GrammarType
+from app.models import Category, Vocab, English, Lexical, Grammar, vocab_english_assoc
 import sys
 import json
+
+db.create_all()
 
 class dictionary_parser:
 	def __init__(self, dict_file):
 		#Read in json vocab file
 		try:
-		    with open(dict_file, encoding='utf-8') as json_content:  # UTF-8 encoding for special latin characters
-		        self.vocab_content = json.load(json_content)
+			with open(dict_file, encoding='utf-8') as json_content:  # UTF-8 encoding for special latin characters
+				self.vocab_content = json.load(json_content)
 		except json.decoder.JSONDecodeError:
-		    print('The given JSON input file is invalid. Try validating it at https://jsonlint.com.')
-		    sys.exit(0)
+			print('The given JSON input file is invalid. Try validating it at https://jsonlint.com.')
+			sys.exit(0)
 
 	def create_vocabulary_db(self):
 
-		cat_db = []
-		eng_db = []
-		lex_db = []
-		typ_db = []
-
-		vocab_db = []
-
-		vocab_lex_db = []
-		vocab_typ_db= []
-
-		vocab_id = 0
-
 		for category, category_value in self.vocab_content.items():
-			cat_db.append(category)
+			# check if category exists
+			print(category)
+			cat_exist = db.session.query(Category.name).filter(Category.name==category).count()
+			print(cat_exist)
+
+			# add new Category record
+			if not cat_exist:
+				cdb = Category(name=category)
+				db.session.add(cdb)
+				print(cdb)
 
 			for vocab in category_value:
-				ee = vocab["ewe"][0]
-				
-				for en in vocab["eng"]:
-					eng_db.append(dict(vocab_ID=vocab_id, eng=en))
+				# retrieve Vocab ID if word already exists in database
+				for ee in vocab["ewe"]:
+					vocab_db_id = db.session.query(Vocab.id).filter(Vocab.ewe==ee).count()
 
-					for lex in vocab["lexical"]:
-						if lex not in lex_db:
-							lex_db.append(lex)
-						vocab_lex_db.append(dict(vocab_ID=vocab_id, lexical=lex))
+					# if vocab is not in database, create new Vocab record
+					if not vocab_db_id:
+						vocabdb = Vocab(ewe=ee, category=category, lexical=vocab["lexical"], audio=vocab["audio"], image=vocab["image"])
 
-						for typ in vocab["type"]:
-							if typ not in typ_db:
-								typ_db.append(typ)
-							vocab_typ_db.append(dict(vocab_ID=vocab_id, type=typ))
+						for en in vocab["eng"]:
+							eng_exist = db.session.query(English.word).filter(English.word==en).count()
+							if not eng_exist:
+								engdb = English(word=en)
+								db.session.add(engdb)
+							vocabdb.english_words.append(en)
 
-				vocab_db.append(dict(id=vocab_id, ewe=ee, category=category, audio=vocab["audio"], image=vocab["image"]))
+						for gram in vocab["type"]:
+							gram_exist = db.session.query(Grammar.name).filter(Grammar.name==gram).count()
+							if not gram_exist:
+								gramdb = Grammar(name=gram)
+								db.session.add(gramdb)
+							vocabdb.grammar_types.append(gram)
 
-				vocab_id = vocab_id + 1
+						db.session.add(vocabdb)
+					
+					else: # if vocab with matching ewe word is in databse
 
-		for unit in vocab_db:
-			print(unit)
+						lex_exist = db.session.query(Lexical.name).filter(Lexical.name==vocab["lexical"]).count()
 
-	def add_database_entry(self, values, databse_name):
-		pass
+						if not lex_exist:
+							lexdb = Lexical(name=vocab["lexical"])
+							vocabdb = Vocab(ewe=ee, category=category, lexical=vocab["lexical"], audio=vocab["audio"], image=vocab["image"])
 
-	def create_sentence_db(self):
-		pass
-	"""
-	Read in json sentence file
-	    for each dictionary item 
-	        add to DB
+							db.session.add(lexdb)
+							db.session.add(vocabdb)
 
-	"""
+						else:
+							# check if here is a Vocab record with same ewe and same lexical fields
+							vocabdb = db.session.query(Vocab).filter(Vocab.ewe==ee, Vocab.lexical==vocab["lexical"])
+		#TEST
+							if vocabdb.count():
 
-dp = dictionary_parser("C:\\Users\\mouse\\Dropbox\\Ewe_Project\\learnapp\\lesson_data\\vocabulary_test.json")
+								for en in vocab["eng"]:
+									eng_exist = db.session.query(English).filter(English.word==en).count()
+
+									#if en is not in database, add English record and add to english_words
+									if not eng_exist:
+										engdb = English(word=en)
+										db.session.add(engdb)
+										vocabdb.first().english_words.append(en)
+									else:
+										en_in_vocab = db.session.query(Vocab).filter(en in Vocab.english_words).count()
+										if not en_in_vocab:
+											vocabdb.first().english_words.append(en)
+
+								for typ in vocab["type"]:
+									gram_exist = db.session.query(Grammar).filter(Grammar.name==typ).count()
+
+									if not gram_exist:
+										gramdb = Grammar(name=typ)
+										db.session.add(gramdb)
+										vocabdb.first().grammar_types.append(typ)
+									else:
+										typ_in_vocab = db.session.query(Vocab).filter(typ in Vocab.grammar_types).count()
+										if not typ_in_vocab:
+											vocabdb.first().grammar_types.append(typ)
+
+		#db.session.commit()
+
+	def check_exists(self, condition):
+		record_id = db.session.query(exists().where(condition))
+		return record_id
+
+
+#dp = dictionary_parser("C:\\Users\\mawusi\\Dropbox\\Ewe_Project\\learnapp\\lesson_data\\vocabulary_test.json")
+dp = dictionary_parser("D:\\Dropbox\\Ewe_Project\\learnapp\\lesson_data\\vocabulary_test.json")
 
 dp.create_vocabulary_db()
