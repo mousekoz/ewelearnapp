e3cea94db47a7758f3de25b642ff30ffb398a46e
Mawusi Blewuada <mousekoz13@yahoo.co.uk>
2018-04-08 18:56:20 +0100

    Removed 'logic' import

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
 # Create configuration object from the Config class
 app.config.from_object(Config)
+
 # Create database instance
 db = SQLAlchemy(app)
+
 # Create an instance of the database migration engine
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
-from app import routes, logic
+from app import routes, models
